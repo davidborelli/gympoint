0a0e26b249eeb31c4fc90d311adb207b4bb67ecf
David <david_borelli@live.com>
2019-12-21 20:24:38 -0300

    Routes Configuration

--- frontend/gympoint/src/App.js
+++ frontend/gympoint/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from '~/services/history';
+import Routes from '~/routes';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
