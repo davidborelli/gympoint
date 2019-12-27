2a5578644f18cf6827b20292036b98cb1b91d404
David <david_borelli@live.com>
2019-12-27 06:44:23 -0300

    Configuration Login Route for Dashboard

--- mobile/gympointapp/src/routes.js
+++ mobile/gympointapp/src/routes.js
@@ -1,5 +1,22 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
 
-export default createAppContainer(createSwitchNavigator({ SignIn }));
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
