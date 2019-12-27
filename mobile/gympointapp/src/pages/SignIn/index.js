a7c2d1e45e8b4cafde79f812c2e314fa6cc03dd8
David <david_borelli@live.com>
2019-12-27 05:32:17 -0300

    Configure Reactotron and Redux

--- mobile/gympointapp/src/pages/SignIn/index.js
+++ mobile/gympointapp/src/pages/SignIn/index.js
@@ -8,6 +8,8 @@ import logo from '~/assets/logosplash.png';
 import * as S from './styles';
 
 export default function SignIn() {
+  const handleSubmit = () => {};
+
   return (
     <Background>
       <S.Container>
@@ -17,9 +19,13 @@ export default function SignIn() {
           <S.FormInput
             keyboardType="numeric"
             placeholder="Informe seu ID de cadastro"
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
-          <S.SubmitButton onPress={() => {}}>Entrar no sistema</S.SubmitButton>
+          <S.SubmitButton onPress={handleSubmit}>
+            Entrar no sistema
+          </S.SubmitButton>
         </S.Form>
       </S.Container>
     </Background>
---
+++ mobile/gympointapp/src/store/modules/user/sagas.js
@@ -0,0 +1,33 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = {
+      // Unir 2 objetos
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Sucesso!', 'Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    Alert.alert(
+      'Falha na atualização',
+      'Houve um erro ao atualizar perfil, confira seus dados'
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
